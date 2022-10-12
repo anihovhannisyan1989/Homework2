16e9e276dcab1802391672dea210921952cc5d4d
anihovhannisyan1989 <ani.hovhannisyan1989@yahoo.com>
2022-10-12 23:15:17 +0400

    "binary search correct version"

--- binarySearchRecursive.js
+++ binarySearchRecursive.js
@@ -1,17 +1,15 @@
 let arr = [-8, -5, -1, 0, 1, 2, 3, 4, 5, 6, 7];
-let key = 2;
+let key = 5;
+let start = 0;
+let end = arr.length - 1;
 
-function binSearchRec(arr, key){
-    let start = 0;
-    let end = arr.length - 1;
-    function check(start, end){
+function binSearchRec(start, end){
+
         if(start > end) return -1;
         let mid = Math.floor((start + end) / 2);
         if(key == arr[mid]) return mid;
-        if(key > arr[mid]) return check(mid + 1, end);
-        return check(start, mid - 1);
-    }
-    return check(start, end);
+        if(key > arr[mid]) return binSearchRec(mid + 1, end);
+        return binSearchRec(start, mid - 1);
 }
 
-console.log("Binary search result - ", binSearchRec(arr, key));
+console.log("Binary search result - ", binSearchRec(start, end));
